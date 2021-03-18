43327d671aac770d7a7ba78d11d4d1fb02139032
KarenGrigoryan <karengrigoryan060@gmail.com>
2021-03-18 11:21:13 +0400

    add update username functionality

--- src/user/user-slice.ts
+++ src/user/user-slice.ts
@@ -1,12 +1,13 @@
-import { Action, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { UserModel } from './user-model';
+import { map } from 'rxjs/operators';
 
 export interface UserState {
   users: UserModel[];
 }
 
 const initialState: UserState = {
-  users: []
+  users: [],
 };
 
 export const userSlice = createSlice({
@@ -16,8 +17,9 @@ export const userSlice = createSlice({
     addUser(state, action: PayloadAction<UserModel>) {
       state.users.push(action.payload);
     },
-    updateUser(state) {
-      
-    }
-  }
+    updateUser(state, action: PayloadAction<UserModel>) {
+      // index should be passed
+      state.users[1] = (action.payload);
+    },
+  },
 });
--- src/user/user.tsx
+++ src/user/user.tsx
@@ -1,7 +1,8 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getUsersSelector } from './user-selectors';
 import { userSlice } from './user-slice';
+import { UserModel } from 'user/user-model';
 
 type Props = {
 };
@@ -10,20 +11,45 @@ export const User: FunctionComponent<Props> = props => {
     const usersInStore = useSelector(getUsersSelector);
     const dispatch = useDispatch();
 
-    const onAddUserClick = () => {
+    const  [userModel, setUser]= useState<UserModel>({ id:0, name:"sd", userName:"werdfews", sureName:"erwfdws"});
+
+  const onAddUserClick = () => {
         dispatch(userSlice.actions.addUser({
+            id: usersInStore.length + 1,
             userName: 'userName',
             name: 'name',
-            surnName: 'surName'
+            sureName: 'sureName'
         }));
     };
 
-    return (
+  const updateUsername = () => {
+    setUser(userModel);
+    dispatch(userSlice.actions.updateUser({
+      id:userModel.id,
+      userName: userModel.userName,
+      name: userModel.name,
+      sureName: userModel.sureName
+    }))
+  }
+
+  function usernameOnChange(value: string) {
+    console.log(value);
+    userModel.userName = value;
+    setUser({id: userModel.id, userName: value, sureName: userModel.sureName, name: userModel.name});
+  }
+
+  return (
         <div className="user">
             <button onClick={onAddUserClick}>Add user</button>
             {usersInStore.map((user, i) =>
-                <div key={i}>{user.name}</div>
+                <div key={i}>
+                  <div>{user.id}</div>
+                  <div>{user.userName}</div>
+                </div>
             )}
+            <input onChange={e => usernameOnChange(e.target.value)}
+            />
+            <button onClick={updateUsername}>Update</button>
         </div>
     );
 };
